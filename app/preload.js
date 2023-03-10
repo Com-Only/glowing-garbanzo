b4da689168e65baef5f777ba38eca8d543f243a7
nicolasstrands <strands.media@gmail.com>
2023-03-10 22:34:08 +0400

    feat: handling macOS behavior

--- app/preload.js
+++ app/preload.js
@@ -7,14 +7,28 @@ process.once("loaded", () => {
         {
             shellOpenPath: (url) => {
                 console.log(url)
-                shell.openPath(url)
+                if (process.platform === 'win32') {
+                    shell.openPath(url)
+                } else {
+                    // replace backslashes with forward slashes
+                    url = url.replace(/\\/g, '/')
+
+                    // prefix with smb://
+                    url = 'smb:' + url
+                    shell.openExternal(url)
+                }
             },
             shellOpenExternal: (url) => {
-                // remove focus on Electron window
-                ipcRenderer.send('blur-window')
+                // check if platform is windows
+                if (process.platform === 'win32') {
+                    // remove focus on Electron window
+                    ipcRenderer.send('blur-window')
 
-                // open url or file path
-                shell.openExternal(url, {activate: true})
+                    shell.openExternal(url)
+                } else {
+                    // open url or file path
+                    shell.openExternal(url, {activate: true})
+                }
             },
             getCurrentUser: () => {
                 return os.userInfo().username;
